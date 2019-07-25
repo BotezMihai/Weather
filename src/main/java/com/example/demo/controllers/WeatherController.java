6d65c97d52f5743de95a1a767d21d9fdf97a4925
BotezMihai <mihai_b9812@yahoo.ro>
2019-07-25 22:23:52 +0300

    dto

--- src/main/java/com/example/demo/controllers/WeatherController.java
+++ src/main/java/com/example/demo/controllers/WeatherController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dto.WeatherDto;
 import com.example.demo.service.WeatherService;
 import com.google.gson.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.BeanUtils;
 
 @RestController
 @RequestMapping("/weather")
@@ -16,13 +18,23 @@ public class WeatherController {
 
     @RequestMapping(value = "/{place}", method = RequestMethod.GET)
     public ResponseEntity<String> getWeatherNow(@PathVariable("place") String place) {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.setLenient().create();
         String weather = weatherService.getWeatherNow(place);
         JsonParser parser = new JsonParser();
         JsonObject jo = parser.parse(weather).getAsJsonObject();
         if (jo.has("code"))
             return new ResponseEntity<>(weather, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(weather, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/dto/{place}", method = RequestMethod.GET)
+    public WeatherDto getDtoWeatherNow(@PathVariable("place") String place) {
+        String weather = weatherService.getWeatherNow(place);
+        JsonParser parser = new JsonParser();
+        JsonObject jo = parser.parse(weather).getAsJsonObject();
+        if (!jo.has("code"))
+            return new WeatherDto(jo.get("main").getAsString(), jo.get("description").getAsString(), jo.get("temperature").getAsDouble(), jo.get("umidity").getAsDouble(), jo.get("windSpeed").getAsDouble(), jo.get("clouds").getAsDouble(), jo.get("country").getAsString(), jo.get("city").getAsString(), "ok");
+        return new WeatherDto("not found");
+
+
     }
 }
---
+++ src/main/java/com/example/demo/dto/WeatherDto.java
@@ -0,0 +1,109 @@
+package com.example.demo.dto;
+
+public class WeatherDto {
+    private String main;
+    private String description;
+    private double temperature;
+    private double umidity;
+    private double windSpeed;
+    private double clouds;
+    private String country;
+    private String city;
+    private String status;
+
+    public WeatherDto(String main, String description, double temperature, double umidity, double windSpeed, double clouds, String country, String city, String status) {
+        this.main = main;
+        this.description = description;
+        this.temperature = temperature;
+        this.umidity = umidity;
+        this.windSpeed = windSpeed;
+        this.clouds = clouds;
+        this.country = country;
+        this.city = city;
+        this.status = status;
+    }
+
+    public WeatherDto(String status) {
+        this.main = null;
+        this.description = null;
+        this.temperature = 0;
+        this.umidity = 0;
+        this.windSpeed = 0;
+        this.clouds = 0;
+        this.country = null;
+        this.city = null;
+        this.status = status;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public double getUmidity() {
+        return umidity;
+    }
+
+    public void setUmidity(double umidity) {
+        this.umidity = umidity;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public double getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(double clouds) {
+        this.clouds = clouds;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
